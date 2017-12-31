960e25b5ebf606ae1af3855f961756c1d01080a2
roe-el <rogar28@gmail.com>
2017-12-31 05:30:43 -0600

    Remove functions for X and O, replace with new_game function.

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
 $(document).ready(function() {
     'use strict';
+    console.log(player1XO+ " " + player2XO);
     var $btns = '.top-left-btn,.top-middle-btn,.top-right-btn,.middle-left-btn,.middle-middle-btn,.middle-right-btn,.bottom-left-btn,.bottom-middle-btn,.bottom-right-btn';
-    var player1XO = 'l';
-    var player2XO = null;
-    var $topValues = null;
-    var $middleValues = null;
-    var $bottomValues = null;
-    var $leftValues = null;
-    var $middleVerticalValues = null;
-    var $rightValues = null;
-    var $acrossRight = null;
-    var $acrossLeft = null;
+    var player1XO, player2XO, $topValues, $middleValues, $bottomValues, $leftValues, $middleVerticalValues, $rightValues, $acrossRight, $acrossLeft = null;
+
     var $values = ['.top-left-btn', '.top-middle-btn', '.top-right-btn', '.middle-left-btn', '.middle-middle-btn', '.middle-right-btn', '.bottom-left-btn', '.bottom-middle-btn', '.bottom-right-btn'];
     var random = 0;
     var win = false;
     var count = 0;
-    $('.x-btn').on('click', function() {
+    $('.new_game').on('click', function() {
         player1XO = 'X';
         player2XO = 'O';
         $($btns).text('');
     })
-    $('.o-btn').on('click', function() {
-        player1XO = 'O';
-        player2XO = 'X';
-        $($btns).text('');
-    })
+    
     $($btns).on('click', function() {
         if (player1XO === null || player2XO === null) {
             alert('Choose a mark!');
             return;
         }
         $(this).text(player1XO);
-        count++;
+        
 
         checkForWin();
 
@@ -51,33 +40,42 @@ $(document).ready(function() {
         $rightValues = $('.top-right-btn,.middle-right-btn,.bottom-right-btn').text();
         $acrossLeft = $('.top-left-btn,.middle-middle-btn,.bottom-right-btn').text();
         $acrossRight = $('.top-right-btn,.middle-middle-btn,.bottom-left-btn').text();
-        if ($acrossRight === 'XXX' || $acrossRight === 'OOO' || $acrossLeft === 'XXX' || $acrossLeft === 'OOO' || $topValues === 'XXX' || $topValues === 'OOO' || $middleValues === 'XXX' || $middleValues === 'OOO' || $bottomValues === 'XXX' || $bottomValues === 'OOO' || $rightValues === 'XXX' || $rightValues === 'OOO' || $middleVerticalValues === 'XXX' || $middleVerticalValues === 'OOO' || $leftValues === 'XXX' || $leftValues === 'OOO') {
+        if ($acrossRight === 'XXX' || $acrossLeft === 'XXX' || $topValues === 'XXX' || $middleValues === 'XXX' || $bottomValues === 'XXX' || $rightValues === 'XXX' || $middleVerticalValues === 'XXX' || $leftValues === 'XXX') {
             win = true;
             count = 0;
-            setTimeout(function(){alert('Winner');
-            	$($btns).text('');},500);
-            
+            setTimeout(function() {
+                alert('Good job human :)');
+                $($btns).text('');
+            }, 500);
+
+            return;
+        } else if ($acrossRight === 'OOO' || $acrossLeft === 'OOO' || $topValues === 'OOO' || $middleValues === 'OOO' || $bottomValues === 'OOO' || $rightValues === 'OOO' || $middleVerticalValues === 'OOO' || $leftValues === 'OOO') {
+            win = true;
+            count = 0;
+            setTimeout(function() {
+                alert('Maybe next time human.');
+                $($btns).text('');
+            }, 500);
+
             return;
         }
     }
 
     function computerMove() {
-    	if(count < 5 && win === false){
-    		        console.log('Count:' + count);
-        random = getRandomIntInclusive(8);       
-        if ($($values[random]).text() === '') {
-            if (player1XO === 'O') {
-                $($values[random]).text('X');
-            } else $($values[random]).text('O');
-        } 
-        else {computerMove();}
-        checkForWin();
-    }
-    else{
-    	alert('Tie');
-    	count=0;
-    	$($btns).text('');
-    }
+        if (count < 5 && win === false) {
+            console.log('Count:' + count);
+            random = getRandomIntInclusive(8);
+            if ($($values[random]).text() === '') {
+                if (player1XO === 'O') {
+                    $($values[random]).text('X');
+                } else $($values[random]).text('O');
+            } else { computerMove(); }
+            checkForWin();
+        } else {
+            alert('Tie');
+            count = 0;
+            $($btns).text('');
+        }
         return;
     }
 
